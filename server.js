177dd826e0c36bb96fd34f47b4c66aadaa1ac6d0
Heng-Hao Pu <hpu@oswego.edu>
2020-05-27 13:48:58 -0400

    Connect to db

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+const colors = require('colors');
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
+connectDB();
+
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('API running'));
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server started on port ${PORT}`.yellow.bold)
+);
