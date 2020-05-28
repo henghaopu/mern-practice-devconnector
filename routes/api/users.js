a4bc488e3f5a373d698e33856a5b80b1cbaa3c27
Heng-Hao Pu <hpu@oswego.edu>
2020-05-28 11:44:31 -0400

    Validate req body and send error res
    - work on user registration route
    - we want to be able to send data to this route
    - send name, email, and password in order to register a user
    - before we do any logic to deal with db like register a user, we need to handle validation and responses if the user doesn't send the right stuff

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
 /**
- * @route         GET api/users
- * @description   Test route
+ * @route         POST api/users
+ * @description   Register user
  * @access        Public
  */
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('User route');
+  }
+);
 
 module.exports = router;
