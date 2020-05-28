d1055a09f46f09f08ca2deb3931b460d0f683ac9
Heng-Hao Pu <hpu@oswego.edu>
2020-05-27 21:30:41 -0400

    Create routes
     - https://expressjs.com/en/starter/basic-routing.html
     - a router determines how an express app responds to a client request to a particular endpoint/uri/path and a specific HTTP request method(GET/POST/...)

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('API running'));
 
+// Mount/connect routers to their own specific uri/path/endpoint
+app.use('/api/v1/users', require('./routes/api/users'));
+app.use('/api/v1/auth', require('./routes/api/auth'));
+app.use('/api/v1/profile', require('./routes/api/profile'));
+app.use('/api/v1/posts', require('./routes/api/posts'));
+
 app.listen(PORT, () =>
   console.log(`Server started on port ${PORT}`.yellow.bold)
 );
