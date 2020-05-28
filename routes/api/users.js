bdeab42f9709f5947f066ed774585f81002bcdfa
Heng-Hao Pu <hpu@oswego.edu>
2020-05-28 18:31:24 -0400

    Encrypt password using bcrypt and save data to db

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../ models/User');
@@ -33,7 +34,9 @@ router.post(
       // See if user exists
       let user = await User.findOne({ email });
       if (user) {
-        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
       }
 
       // Get users gravatar
@@ -50,11 +53,16 @@ router.post(
         password,
       });
 
-      // Encript password
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      // Save user to db
+      await user.save();
 
       // Return jsonwebtoken
 
-      res.send('User route');
+      res.send('User registered');
     } catch (err) {
       console.log(err.message);
       res.status(500).send('Server error');
