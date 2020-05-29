fd5677bf092c9a3a947d910b39c6169594782f6f
Heng-Hao Pu <hpu@oswego.edu>
2020-05-29 16:33:04 -0400

    Implement auth route to respond authorized user infomation

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const User = require('../../ models/User');
 /**
  * Just by putting auth as the second argement makes this route protected
  * @route         GET api/v1/auth
  * @description   Test route
  * @access        Public
  */
-router.get('/', auth, (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
